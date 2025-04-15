35a4349288fe7a00a1a03a41a16fafc8cae5a4fc
moven0831 <moven0831@gmail.com>
2025-04-15 11:28:06 +0800

    ci: specify platform support for MoproFFI and update package structure

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "MoproFFI",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v15),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -23,10 +27,6 @@ let package = Package(
         .binaryTarget(
             name: "mopro",
             path: "Sources/MoproiOSBindings/MoproBindings.xcframework.zip"
-        ),
-        .package(
-            name: "mopro",
-            path: "Sources/MoproiOSBindings/MoproBindings.xcframework"
         ),
         .testTarget(
             name: "MoproFFITests",
